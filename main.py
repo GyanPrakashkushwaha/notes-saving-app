0cfd46943dca00edf356a079d5a2879420e1f1a2
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-07-31 14:35:43 +0530

    src

--- main.py
+++ main.py
@@ -16,12 +16,34 @@ client = MongoClient(SECRET_KEY_MONGO_DB)
 
 
 @app.get("/", response_class=HTMLResponse) 
-async def read_item(request: Request): # this function is a function of fastapi library to read stuff in the http requests.
-    docs = client.notesSaving.notesSavingApp.find_one({})
-    print(docs)
-    return templates.TemplateResponse("index.html", {"request": request, 'docs' :docs}) # by this I am rendering everything in my webpage.
+async def read_item(request: Request):
+    docs = list(client.notesSaving.notesSavingApp.find({}))
+
+    docs_new = []
+    for i in docs:
+        # Use a try-except block to handle conversion to integer and set a default value of 0
+        try:
+            ratings = int(i["ratings"])
+        except (ValueError, KeyError):
+            ratings = 0  # Set a default value of 0 if "ratings" is missing or cannot be converted to an integer
+
+        docs_new.append({
+            'id': i["_id"],
+            'name': i["name"],
+            'ratings': ratings,
+            'price': i['price'],
+            'imgURL': i['imgURL'],
+            # 'storage_ram': i['storage_ram'],
+            # 'camera': i['camera'],
+            # 'oS_Processor': i['oS_Processor'],
+            # 'display': i['display'],
+            # 'network': i['network'],
+            # 'battery': i['battery'],
+        })
+
+    return templates.TemplateResponse("index.html", {"request": request, 'docs': docs_new})
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
